4bad26dc9d7d7fb7b508136c237b8366d7dde8fc
Charles Shapiro <charles.shapiro@tomshiro.org>
2024-11-05 10:55:27 -0500

    Scale to proper playdate format

--- doc/convert_to_gif.sh
+++ doc/convert_to_gif.sh
@@ -18,7 +18,7 @@ fi
 echo $INFILE $OUTFILE
 set -x
 ffmpeg             -i ${INFILE} \
-       -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+       -vf "fps=10,scale=400:240:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
        -loop 0 ${OUTFILE}
       
 
